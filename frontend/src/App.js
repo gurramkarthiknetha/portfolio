96a2841e96adec7053052feced7c18e6fc560ab5
Gurram Karthik <167804249+gurramkarthiknetha@users.noreply.github.com>
2025-06-29 16:03:18 +0530

    added seo-1

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -12,31 +12,12 @@ function App() {
   return (
     <div className="App">
       <Helmet>
-        <title>Karthik Gurram | Portfolio</title>
-        <meta name="description" content="Full Stack Developer with expertise in React, Node, and more." />
-        <meta name="keywords" content="Karthik,Gurram karthik, Portfolio, React Developer, Full Stack, Web Developer" />
+        <title>Karthik Gurram | Full Stack Developer</title>
+        <meta name="description" content="Portfolio of Karthik Gurram, a full stack web developer with experience in React, Node, and more." />
+        <meta name="keywords" content="Karthik Gurram, web developer, React, portfolio, full stack" />
         <meta property="og:title" content="Karthik Gurram Portfolio" />
-        <meta property="og:description" content="Showcasing projects and skills of Karthik Gurram." />
         <meta property="og:url" content="https://gurramkarthik.vercel.app/" />
         <meta property="og:type" content="website" />
-        <link rel="canonical" href="https://gurramkarthik.vercel.app/" />
-        <script type="application/ld+json">{`
-          {
-            "@context": "http://schema.org",
-            "@type": "Person",
-            "name": "Karthik Gurram",
-            "url": "https://gurramkarthik.vercel.app",
-            "sameAs": [
-              "https://www.linkedin.com/in/karthik-gurram-b7516b293/",
-              "https://github.com/KarthikGurram24"
-            ],
-            "jobTitle": "Full Stack Developer",
-            "worksFor": {
-              "@type": "Organization",
-              "name": "VNRVJIET"
-            }
-          }
-        `}</script>
       </Helmet>
       <NavBar />
       <Banner />
